eec10c6a4f6c14990669ae0ca4d8c4d544058a57
root <root@fishbase.cafs>
2018-10-18 23:45:29 +0800

    Nginx Fix W2

--- FishBase-Dockers/nginx/dockerrun.sh
+++ FishBase-Dockers/nginx/dockerrun.sh
@@ -3,5 +3,6 @@
 if [ -n "$WEB2DOMAIN" ]; then
         cp -f /template.conf "/etc/nginx/conf.d/$WEB2DOMAIN.conf"
         sed -i "s/{WEBDOMAIN}/$WEB2DOMAIN/g" "/etc/nginx/conf.d/$WEB2DOMAIN.conf"
+        sed -i "s/default_server/ /g" "/etc/nginx/conf.d/$WEB2DOMAIN.conf"
 fi
 
