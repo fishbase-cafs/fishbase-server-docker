b5f68db0d5f86a2b28badea166af09ddf6cef593
root <root@fishbase.cafs>
2018-10-19 00:21:44 +0800

    Fix Log Bug1

--- FishBase-Dockers/nginx/dockerentry.sh
+++ FishBase-Dockers/nginx/dockerentry.sh
@@ -7,6 +7,7 @@ fi
 mkdir -p /etc/nginx/conf.d
 cp -f /template.conf "/etc/nginx/conf.d/$WEBDOMAIN.conf"
 sed -i "s/{WEBDOMAIN}/$WEBDOMAIN/g" "/etc/nginx/conf.d/$WEBDOMAIN.conf"
+sed -i "s/{LOGTAG}/ /g" "/etc/nginx/conf.d/$WEBDOMAIN.conf"
 
 /usr/local/bin/docker-run.sh > /dev/null 2>&1 &
 
