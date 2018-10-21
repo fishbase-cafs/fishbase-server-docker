3b2ba43fde51b4f75be66d251944411c156fc364
root <root@fishbase.cafs>
2018-10-21 17:00:06 +0800

    Fix bugs in Create SSL with API01

--- FishBase-Dockers/nginx/https-create.sh
+++ FishBase-Dockers/nginx/https-create.sh
@@ -5,8 +5,33 @@ if [ -z "$cr" ]; then
 	certbot register --agree-tos --email webmaster@$WEBDOMAIN
 fi
 echo == Did SSL ==
+if [ -d "/etc/letsencrypt/live/$WEBDOMAIN" ]; then
+	mv "/etc/letsencrypt/live/$WEBDOMAIN" "/etc/letsencrypt/live/$WEBDOMAIN-bak"
+fi
+if [ -d "/etc/letsencrypt/archive/$WEBDOMAIN" ]; then
+        mv "/etc/letsencrypt/archive/$WEBDOMAIN" "/etc/letsencrypt/archive/$WEBDOMAIN-bak"
+fi
+if [ -f "/etc/letsencrypt/renewal/$WEBDOMAIN.conf" ]; then
+        mv "/etc/letsencrypt/renewal/$WEBDOMAIN.conf" "/etc/letsencrypt/renewal/$WEBDOMAIN.conf.bak"
+fi
 certbot certonly --webroot -w /etc/letsencrypt/acme -d $WEBDOMAIN -d www.$WEBDOMAIN -n
-/usr/sbin/nginx -s reload
+if [ -d "/etc/letsencrypt/live/$WEBDOMAIN" ]; then
+	rm -rf "/etc/letsencrypt/live/$WEBDOMAIN-bak"
+else
+	mv "/etc/letsencrypt/live/$WEBDOMAIN-bak" "/etc/letsencrypt/live/$WEBDOMAIN"
+fi
+if [ -d "/etc/letsencrypt/archive/$WEBDOMAIN" ]; then
+        rm -rf "/etc/letsencrypt/archive/$WEBDOMAIN-bak"
+else
+        mv "/etc/letsencrypt/archive/$WEBDOMAIN-bak" "/etc/letsencrypt/archive/$WEBDOMAIN"
+fi
+if [ -f "/etc/letsencrypt/renewal/$WEBDOMAIN.conf" ]; then
+        rm -rf "/etc/letsencrypt/renewal/$WEBDOMAIN.conf.bak"
+else
+        mv "/etc/letsencrypt/renewal/$WEBDOMAIN.conf.bak" "/etc/letsencrypt/renewal/$WEBDOMAIN.conf"
+fi
+
+nginx -s reload
 
 #certbot certonly -d www.$WEBDOMAIN -d $WEBDOMAIN --agree-tos --email webmaster@$WEBDOMAIN --manual --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory
 
--- FishBase-Dockers/nginx/https-update.sh
+++ FishBase-Dockers/nginx/https-update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-killall certbot
+killall certbot > /dev/null 2>&1
 certbot renew --quiet --renew-hook "/usr/sbin/nginx -s reload" &
 date > /tmp/lastbot
